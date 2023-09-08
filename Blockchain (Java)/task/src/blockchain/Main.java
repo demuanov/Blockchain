0cc1e98b69857d3dd17d3ab54e4309178de3c3b7
demunaov <demuanov@gmail.com>
2023-09-08 15:00:23 +0300

    solve task

--- Blockchain (Java)/task/src/blockchain/Main.java
+++ Blockchain (Java)/task/src/blockchain/Main.java
@@ -1,24 +1,14 @@
 package blockchain;
 
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
+      BlockChain  blockChain = new BlockChain();
 
-
-            try {
-                // Parses the date
-                String result = String.valueOf(LocalDate.parse("2018-01-12"));
-               Block kek = new Block();
-
-                System.out.println("Some output: " + kek);
-
-                // Prints the date with year
-                System.out.println("The date with month is: " + result);
-            }
-            catch (Exception e) {
-                System.out.println(e);
-            }
-
+       blockChain.addBlock();
+       blockChain.addBlock();
+       blockChain.addBlock();
+       blockChain.addBlock();
+       blockChain.addBlock();
+       blockChain.getAllBlocks();
     }
 }
