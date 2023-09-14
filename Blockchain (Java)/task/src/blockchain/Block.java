c640a4cab658456201d958374471692950b30282
demunaov <demuanov@gmail.com>
2023-09-14 19:11:49 +0300

    Block step 2

--- Blockchain (Java)/task/src/blockchain/Block.java
+++ Blockchain (Java)/task/src/blockchain/Block.java
@@ -1,18 +1,21 @@
 package blockchain;
 
 import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Block {
     private final int id;
     private final long timeStamp;
     private final String previousBlockHash;
     private final String blockHash;
+    private float blockGenerationTime;
+    private String magicNumber;
 
-    public Block(int id, String previousBlockHash) {
+    public Block (int id, String previousBlockHash, int numberOfZeros) {
         this.id = id;
         this.timeStamp = new Date().getTime();
         this.previousBlockHash = previousBlockHash;
-        this.blockHash = StringUtil.applySha256(String.valueOf(id));
+        this.blockHash = setHash(numberOfZeros);
     }
 
     public String getBlockHash() {
@@ -23,10 +26,30 @@ public class Block {
         return "Block: " +
                 "\nId: " + id +
                 "\nTimestamp: " + timeStamp +
+                "\nMagic number: " + magicNumber +
                 "\nHash of the previous block: \n"+ previousBlockHash +
-                "\nHash of the block: \n" + blockHash + "\n";
+                "\nHash of the block: \n" + blockHash +
+                "\nBlock was generating for " + blockGenerationTime + " seconds" + "\n";
     }
 
+    private String setHash(int numberOfZeros){
+        String zeros = "0".repeat(numberOfZeros);
+        this.magicNumber = String.valueOf(ThreadLocalRandom.current().nextInt());
+        String hash = StringUtil.applySha256(this.magicNumber);
 
+
+        Long startTime = System.currentTimeMillis();
+        while (!hash.substring(0, numberOfZeros).equals(zeros)) {
+            String generationNumber = String.valueOf(ThreadLocalRandom.current().nextInt());
+            hash = StringUtil.applySha256(generationNumber);
+            this.magicNumber = generationNumber;
+       }
+
+       Long endTime = System.currentTimeMillis();
+
+        this.blockGenerationTime = (endTime - startTime) / 1000F;
+        System.out.println("BLOCK GENERATION TIME ->" + blockGenerationTime);
+       return hash;
+    }
 
 }
